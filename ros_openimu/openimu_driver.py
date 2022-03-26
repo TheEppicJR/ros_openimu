66ef8b349ef8b76ba16aaf53e4f20582bcba8945
TheEppicJR <mantacast4154@gmail.com>
2022-03-25 19:50:23 -0700

    Fixed typo

--- ros_openimu/openimu_driver.py
+++ ros_openimu/openimu_driver.py
@@ -28,8 +28,8 @@ class OpenIMUros(Node):
     def __init__(self):
         self.openimudev = OpenIMU()
         self.openimudev.startup()
-        self.pub_imu: Publisher = self.create_publisher('imu_acc_ar', Imu, queue_size=1)
-        self.pub_mag: Publisher = self.create_publisher('imu_mag', MagneticField, queue_size=1)
+        self.pub_imu: Publisher = self.create_publisher('imu_acc_ar', Imu, 1)
+        self.pub_mag: Publisher = self.create_publisher('imu_mag', MagneticField, 1)
         #read the data - call the get imu measurement data
         self.packetType = 'a2'                       # z1, s1, a1, a2, e1, e2
         
