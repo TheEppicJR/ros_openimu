a9f75c47e6245fdadba78cd2affa5ee47312b2cf
TheEppicJR <mantacast4154@gmail.com>
2022-03-30 16:28:21 -0700

    Correct version

--- ros_openimu/openimu_driver.py
+++ ros_openimu/openimu_driver.py
@@ -219,7 +219,7 @@ def main(args=sys.argv[1:]):
     # defaults args
     loglevel = 'info'
     print_logs = False
-    i=2
+    i=3
 
 
     numeric_level = getattr(logging, loglevel.upper(), None)
